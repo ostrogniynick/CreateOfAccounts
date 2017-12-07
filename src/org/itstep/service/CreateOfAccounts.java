d60425b9fafa0b4fe265866cb6dc812954361fa6
Бог <@LAPTOP-NDG5E9M2>
2017-12-07 09:53:12 +0200

    Допилин полностью рабочий рандомайзер логина

--- src/org/itstep/service/CreateOfAccounts.java
+++ src/org/itstep/service/CreateOfAccounts.java
@@ -9,14 +9,15 @@ public class CreateOfAccounts {
 	public void saveAccount(String NumberAccound ) {	
 		if(NumberAccound.isEmpty()) {
 			return;
-	}
+	}	
+		String[] lettersForMail  = {"q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m","Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"};
 		int numbers = Integer.parseInt(NumberAccound);
 		for(int i =1 ; i<=numbers;i++) {
 		String[] domeins = {"gmail.com", "mail.ru" , "ukr.net.ua"};
-		String password = "psdwd" + getRandomNumber(100000, 999999);
-		String email =  getRandomNumber(1000, 9999) + "@" + getRandomDomein(domeins);
+		String password = getRandomWord(lettersForMail) + getRandomNumber(100000, 999999);
+		String email = getRandomWord(lettersForMail) + "." + getRandomNumber(1000, 9999) + "@" + getRandomDomein(domeins);
 		String line =  email + ", " + password   + ";\n" ;
-		  appendInfoToFile(line,NumberAccound);
+		appendInfoToFile(line,NumberAccound);
 		}
 	}
 	
@@ -51,5 +52,26 @@ public class CreateOfAccounts {
 		 String rndStr = domeins[rndIndex];
 		 return rndStr;
 	}
+	
+	private String getRandomWord(String[] lettersForMail) {
+		int numberOfLetters = (int)(Math.random()*10);
+		String alphabetArray = "" ;
+		for(int i = 0 ; i <= numberOfLetters ; i++) {
+			int rndIndex = (int)(Math.random()*lettersForMail.length);
+			 alphabetArray += lettersForMail[rndIndex];
+		}
+		return alphabetArray;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
